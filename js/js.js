af29ef865bda3fd8f6aa91320feb1b2ca7db3d6e
eshlfrv <eashlaferov@edu.hse.ru>
2023-03-31 12:55:39 +0300

    1

---
+++ js/js.js
@@ -0,0 +1,74 @@
+
+$(document).ready(function() {
+$(".t1").click(function() {
+$('html,body').animate({
+scrollTop: $(".thirdscreen").offset().top},
+'slow');});
+});
+
+
+$(document).ready(function() {
+$(".t2").click(function() {
+$('html,body').animate({
+scrollTop: $(".fifthscreen").offset().top},
+'slow');});
+});
+
+
+$(document).ready(function() {
+$(".t3").click(function() {
+$('html,body').animate({
+scrollTop: $(".sixthscreen").offset().top},
+'slow');});
+});
+
+type="text/javascript">
+$(document).ready(function() {
+    $("#p5").click(function() {
+        $("#card1").show();
+    $("#card1").mouseleave(function() {
+        $("#card1").hide();
+});
+});
+});
+
+type="text/javascript">
+$(document).ready(function() {
+    $("#p5").click(function() {
+        $("#card1").show();
+    $("#card1").mouseleave(function() {
+        $("#card1").hide();
+});
+});
+});
+
+
+$(document).ready(function() {
+    $("#p5").click(function() {
+        $("#card1").show();
+        $('#card1').css('animation-play-state', 'running');
+    $("#card1").mouseleave(function() {
+        $("#card1").hide();
+});
+});
+});
+
+$(document).ready(function() {
+    $("#p6").click(function() {
+        $("#card2").show();
+        $('#card2').css('animation-play-state', 'running');
+    $("#card2").mouseleave(function() {
+        $("#card2").hide();
+});
+});
+});
+
+$(document).ready(function() {
+    $("#p7").click(function() {
+        $("#card3").show();
+        $('#card3').css('animation-play-state', 'running');
+    $("#card3").mouseleave(function() {
+        $("#card3").hide();
+});
+});
+});
